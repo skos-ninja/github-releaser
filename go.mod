dde7b828c5fbd5fba828783ff3a1a9a842823077
Jake Oliver <github@skos.ninja>
2021-12-15 01:37:11 +0000

    Fix exclude repos being fetched by updating dependencies

--- go.mod
+++ go.mod
@@ -5,11 +5,18 @@ go 1.16
 require (
 	github.com/Masterminds/semver v1.5.0
 	github.com/bradleyfalzon/ghinstallation v1.1.1
-	github.com/gin-gonic/gin v1.7.4
+	github.com/gin-gonic/gin v1.7.7
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/google/go-github/v29 v29.0.3 // indirect
 	github.com/google/go-github/v35 v35.3.0
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4
 	github.com/kr/pretty v0.3.0
-	github.com/skos-ninja/config-loader v0.0.0-20210530160620-dfcaee536361
-	github.com/spf13/cobra v1.2.1
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/skos-ninja/config-loader v0.1.0
+	github.com/spf13/cobra v1.3.0
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/sys v0.0.0-20211214234402-4825e8c3871d // indirect
 )
--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/v35/github"
+	"github.com/skos-ninja/config-loader"
 	"github.com/spf13/cobra"
 
-	"github.com/skos-ninja/config-loader"
 	"github.com/skos-ninja/github-releaser/app"
-	. "github.com/skos-ninja/github-releaser/pkg/common"
+	"github.com/skos-ninja/github-releaser/pkg/common"
 	"github.com/skos-ninja/github-releaser/rpc"
 )
 
@@ -23,6 +23,12 @@ var (
 		Args: cobra.ExactArgs(0),
 		RunE: runE,
 	}
+	cfg = &common.Config{
+		Github:          common.Github{},
+		ImpersonateTags: false,
+		Port:            8080,
+		ExcludeRepos:    []string{},
+	}
 )
 
 func init() {
@@ -31,11 +37,11 @@ func init() {
 	config.Init(cmd)
 	cmd.Flags().IntP("port", "p", cfg.Port, "HTTP Listening port")
 	cmd.Flags().BoolP("impersonatetags", "i", cfg.ImpersonateTags, "Impersonate users when tagging")
-	cmd.Flags().Int("appid", int(cfg.Github.AppId), "")
-	cmd.Flags().String("webhooksecret", cfg.Github.WebhookSecretKey, "")
-	cmd.Flags().String("privatekey", cfg.Github.PrivateKey, "")
-	cmd.Flags().String("enterpriseurl", cfg.Github.EnterpriseURL, "")
-	cmd.Flags().StringArray("excluderepos", cfg.ExcludeRepos, "List of repos to exclude")
+	cmd.Flags().Int("appid", int(cfg.Github.AppId), "GitHub app id")
+	cmd.Flags().String("webhooksecret", cfg.Github.WebhookSecretKey, "GitHub webhook secret")
+	cmd.Flags().String("privatekey", cfg.Github.PrivateKey, "GitHub app private key")
+	cmd.Flags().String("enterpriseurl", cfg.Github.EnterpriseURL, "GitHub enterprise url")
+	cmd.Flags().StringSliceP("excluderepos", "x", cfg.ExcludeRepos, "List of repos to exclude")
 }
 
 func main() {
@@ -72,7 +78,7 @@ func runE(cmd *cobra.Command, args []string) error {
 	return router.Run(fmt.Sprintf(":%v", cfg.Port))
 }
 
-func setupGithubClient(cfg Github) (*ghinstallation.AppsTransport, *github.Client, error) {
+func setupGithubClient(cfg common.Github) (*ghinstallation.AppsTransport, *github.Client, error) {
 	tr := http.DefaultTransport
 
 	// Due to envs vars loading \n as escaped we need to unescape
